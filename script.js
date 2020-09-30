d20f8086996d7cc4658759c78a1cc6466931fa39
Subham Yadav <subhamy142@gmail.com>
2020-09-30 18:37:01 +0530

    tried new approch

--- script.js
+++ script.js
@@ -1,44 +1,99 @@
-var display=document.getElementById('display_screen');
-var buttons=document.getElementsByClassName('numbers');
-var operand1=0;
-var operand2=null;
-var operator=null;
-function isOperator(value){
-  return value =="+" || value=='-' || value=='*'  || value=="/";
-}
-for(var i=0;i<buttons.length;i++){
-  buttons[i].addEventListener('click',function(){
-    var value = this.getAttribute('data-value');
-    var text = display.textContent.trim();
-    if(isOperator(value)){
-     operand1=parseFloat(text); 
-      operator=value;
-      display.textContent="";
-    }else if(value=="ac"){
-      display.textContent="";
-    }
-    else if(value=="."){
-      if(text.length && !text.includes('.')){
-        display.textContent=text+'.';
-      }
-      else if(value =="%"){
-        operand1=parseFloat(text);
-        operand1=operand1/100;
-        display.textContent=operand1;
-      }
-    }
-    else if(value =='='){
-     operand2=parseFloat(text);
-      var result=eval(operand1+' '+operator+' '+operand2);
-      if(result){
-        display.textContent=result;
-        operand1=result;
-        operand2="null";
-        operator=null;
-      }
-    }
-    else{
-      display.innerText += value;
-    }
-  });
-}
+"use strict";
+var cals=document.getElementsByClassName('calculations')[0];
+
+//all the calculation part
+document.getElementById('one').addEventListener('click',function()
+{
+    cals.innerText+=1;
+});
+
+document.getElementById('two').addEventListener('click',function()
+{
+    cals.innerText+=2;
+});
+
+document.getElementById('three').addEventListener('click',function()
+{
+    cals.innerText+=3;
+});
+
+document.getElementById('four').addEventListener('click',function()
+{
+    cals.innerText+=4;
+});
+
+document.getElementById('five').addEventListener('click',function()
+{
+    cals.innerText+=5;
+});
+
+document.getElementById('six').addEventListener('click',function()
+{
+    cals.innerText+=6;
+});
+
+document.getElementById('seven').addEventListener('click',function()
+{
+    cals.innerText+=7;
+});
+
+document.getElementById('eight').addEventListener('click',function()
+{
+    cals.innerText+=8;
+});
+
+document.getElementById('nine').addEventListener('click',function()
+{
+    cals.innerText+=9;
+});
+
+document.getElementById('zero').addEventListener('click',function()
+{
+    cals.innerText+=0;
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+document.getElementById('point').addEventListener('click',function()
+{
+    cals.innerText+=".";
+});
+
+document.getElementById('percent').addEventListener('click',function()
+{
+    cals.innerText+="%";
+});
+
+document.getElementById('divide').addEventListener('click',function()
+{
+    cals.innerText+="/";
+});
+
+document.getElementById('multiply').addEventListener('click',function()
+{
+    cals.innerText+="*";
+});
+
+document.getElementById('add').addEventListener('click',function()
+{
+    cals.innerText+="+";
+});
+
+document.getElementById('subtract').addEventListener('click',function()
+{
+    cals.innerText+="-";
+});
+
+document.getElementById('ac').addEventListener('click',function()
+{
+    cals.innerText="";
+});
+
+document.getElementById('c').addEventListener('click',function()
+{
+    cals.innerText = cals.innerText.substring(0, cals.innerText.length - 1);
+});
+
+document.getElementById('equal').addEventListener('click',function()
+{
+    cals.innerText=eval(cals.innerText);
+});
