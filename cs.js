c60e6c8105c28a6df558f7ef34c14fbe9e6cfd74
Subham Yadav <subhamy142@gmail.com>
2020-10-10 12:51:15 +0530

    version 2.0.0

--- cs.js
+++ cs.js
@@ -1,99 +1,80 @@
 "use strict";
-var cals=document.getElementsByClassName('calculations')[0];
-
-//all the calculation part
-document.getElementById('one').addEventListener('click',function()
+var cals = document.getElementsByClassName('calculations')[0];
+/* stores all the calculation expressions */
+document.getElementById('one').addEventListener('click', function ()
 {
-    cals.innerText+=1;
+    cals.innerText += 1;
 });
-
-document.getElementById('two').addEventListener('click',function()
+document.getElementById('two').addEventListener('click', function ()
 {
-    cals.innerText+=2;
+    cals.innerText += 2;
 });
-
-document.getElementById('three').addEventListener('click',function()
+document.getElementById('three').addEventListener('click', function ()
 {
-    cals.innerText+=3;
+    cals.innerText += 3;
 });
-
-document.getElementById('four').addEventListener('click',function()
+document.getElementById('four').addEventListener('click', function ()
 {
-    cals.innerText+=4;
+    cals.innerText += 4;
 });
-
-document.getElementById('five').addEventListener('click',function()
+document.getElementById('five').addEventListener('click', function ()
 {
-    cals.innerText+=5;
+    cals.innerText += 5;
 });
-
-document.getElementById('six').addEventListener('click',function()
+document.getElementById('six').addEventListener('click', function ()
 {
-    cals.innerText+=6;
+    cals.innerText += 6;
 });
-
-document.getElementById('seven').addEventListener('click',function()
+document.getElementById('seven').addEventListener('click', function ()
 {
-    cals.innerText+=7;
+    cals.innerText += 7;
 });
-
-document.getElementById('eight').addEventListener('click',function()
+document.getElementById('eight').addEventListener('click', function ()
 {
-    cals.innerText+=8;
+    cals.innerText += 8;
 });
-
-document.getElementById('nine').addEventListener('click',function()
+document.getElementById('nine').addEventListener('click', function ()
 {
-    cals.innerText+=9;
+    cals.innerText += 9;
 });
-
-document.getElementById('zero').addEventListener('click',function()
+document.getElementById('zero').addEventListener('click', function ()
 {
-    cals.innerText+=0;
+    cals.innerText += 0;
 });
-/////////////////////////////////////////////////////////////////////////////////////////////////
-
-document.getElementById('point').addEventListener('click',function()
+/////////////////////////////////////////////////////////////////////
+document.getElementById('point').addEventListener('click', function ()
 {
-    cals.innerText+=".";
+    cals.innerText += ".";
 });
-
-document.getElementById('percent').addEventListener('click',function()
+document.getElementById('ac').addEventListener('click', function ()
 {
-    cals.innerText+="%";
+    cals.innerText = "";
 });
-
-document.getElementById('divide').addEventListener('click',function()
-{
-    cals.innerText+="/";
-});
-
-document.getElementById('multiply').addEventListener('click',function()
-{
-    cals.innerText+="*";
-});
-
-document.getElementById('add').addEventListener('click',function()
-{
-    cals.innerText+="+";
-});
-
-document.getElementById('subtract').addEventListener('click',function()
-{
-    cals.innerText+="-";
-});
-
-document.getElementById('ac').addEventListener('click',function()
-{
-    cals.innerText="";
-});
-
-document.getElementById('c').addEventListener('click',function()
+document.getElementById('c').addEventListener('click', function ()
 {
     cals.innerText = cals.innerText.substring(0, cals.innerText.length - 1);
 });
-
-document.getElementById('equal').addEventListener('click',function()
+document.getElementById('modulo').addEventListener('click', function ()
 {
-    cals.innerText=eval(cals.innerText);
+    cals.innerText += "%";
+});
+document.getElementById('slash').addEventListener('click', function ()
+{
+    cals.innerText += "/";
+});
+document.getElementById('mul').addEventListener('click', function ()
+{
+    cals.innerText += "*";
+});
+document.getElementById('sub').addEventListener('click', function ()
+{
+    cals.innerText += "-";
+});
+document.getElementById('add').addEventListener('click', function ()
+{
+    cals.innerText += "+";
+});
+document.getElementById('equal').addEventListener('click', function ()
+{
+    cals.innerText = eval(cals.innerText);
 });
